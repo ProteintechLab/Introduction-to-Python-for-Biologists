f6589263f6542517f07bb316d5e7cc88adca913e
ProteintechLab <165922658+ProteintechLab@users.noreply.github.com>
2025-11-29 15:50:42 +0000

    Create Error_1.py

---
+++ Debugging Examples/More Examples/Error_1.py
@@ -0,0 +1,15 @@
+# Exercise 3 — fix 5 bugs
+from Bio import SeqIo                 # 1
+def calculate_gc(seq):
+    """Return GC% for a sequence""" 
+    gc = seq.count("G") + seq.count("C")
+    return (gc / len(seq)) * 100
+
+for rec in SeqIO.parse("example.fasta","fasta"):   # 2
+    try:
+        pct = calculate_gc(rec.seq)
+        print(f"{rec.id}: {pct:.2f}%")
+    except ZeroDivisionErro:                       # 3
+        print(f"{rec.id} empty sequence")
+    except:                                        # 4
+        print("Unknown error", e)                  # 5
