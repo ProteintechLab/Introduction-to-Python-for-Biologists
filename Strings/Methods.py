79458fb9b828afcd6bc11ebf4e4e2065ff2f3e3b
ProteintechLab <165922658+ProteintechLab@users.noreply.github.com>
2025-11-29 15:40:08 +0000

    Create Methods.py

---
+++ Strings/Methods.py
@@ -0,0 +1,19 @@
+print("gattaca".capitalize())
+
+print("Gattaca".swapcase())
+
+print("GATTACA".lower()) 
+
+print("gattaca".upper())
+
+print("GATTACA".count("C"))
+
+print("ATG".replace("T","U"))
+
+print("GAPDH/TP53/TDP43".split("/"))
+
+print("  GATTACA.  ".strip())
+
+print("gattaca".find("a"))
+
+print("--".join("gattaca"))
