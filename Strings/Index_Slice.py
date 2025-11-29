76675bce2c8e8a9e5794a25d483aab0d3e69411e
ProteintechLab <165922658+ProteintechLab@users.noreply.github.com>
2025-11-29 15:39:50 +0000

    Update and rename Strings_Part1.py to Index_Slice.py

--- Strings/Index_Slice.py
+++ Strings/Index_Slice.py
@@ -11,23 +11,3 @@ print("The characters from position 0 to 2 inclusive are "+ sequence[0:3])
 
 # Slicing [start:stop:step]
 print("The characters from position 0 to 5 inclusive with a step value of 2 are "+sequence[0:6:2])
-
-print("gattaca".capitalize())
-
-print("Gattaca".swapcase())
-
-print("GATTACA".lower()) 
-
-print("gattaca".upper())
-
-print("GATTACA".count("C"))
-
-print("ATG".replace("T","U"))
-
-print("GAPDH/TP53/TDP43".split("/"))
-
-print("  GATTACA.  ".strip())
-
-print("gattaca".find("a"))
-
-print("--".join("gattaca"))
